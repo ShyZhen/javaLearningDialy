0dab1c29ec2d095fc7b9e93cefcb1301184d16bc
zhenhuaixiu <zhenhuaixiu@sznews.com>
2021-12-06 18:55:21 +0800

    集合基础 - list2

--- src/main/lesson7/CollectTest.java
+++ src/main/lesson7/CollectTest.java
@@ -112,7 +112,45 @@ public class CollectTest {
         //添加元素到末尾 	     速度很快          速度很快
         //指定位置插入/删除     需要移动元素      不需要移动元素
         //内存占用             少              较大
-        List<Integer> linkedList = new LinkedList<>();
+        List<String> linkedList = new LinkedList<>();
+        linkedList.add("index1");
+        linkedList.add("index2");
+        linkedList.add("index2");
+        linkedList.set(2, "index3");
+        System.out.println(linkedList);
+
+        // for遍历list：一是代码复杂，二是因为get(int)方法只有ArrayList的实现是高效的，换成LinkedList后，索引越大，访问速度越慢
+        // 所以推荐使用迭代器 Iterator
+        Integer size = linkedList.size();
+        for (int i = 0; i < size; i++) {
+            String s = linkedList.get(i);
+            System.out.println(s);
+        }
+
+        // Iterator 是所有集合类都能使用的。他知道如何遍历一个list，不同的list类型返回的实例对象也是不同的，总有最高的访问效率
+        // Iterator对象有两个方法：boolean hasNext()判断是否有下一个元素，E next()返回下一个元素
+        Iterator<String> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        // 以上两种方式等价于for each
+        // Java的for each循环本身就可以帮我们使用Iterator遍历
+        for (String s : linkedList) {
+            System.out.println(s);
+        }
+
+
+
+
+
+
+
+
+        // 装入数组 array
+        Integer size2 = linkedList.size();
+        String[] array = linkedList.toArray(new String[5]);  // 一般会存入size，正好的个数。如果大于size，补null
+        System.out.println(Arrays.toString(array));    // [index1, index2, index3, null, null]
     }
 
 
