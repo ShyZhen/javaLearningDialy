a6647341cf322a2903bd2da753208ef63853557b
zhenhuaixiu <zhenhuaixiu@sznews.com>
2022-03-03 17:36:01 +0800

    配置文件 Properties：包括读、写、覆盖load、指定读取的字符集utf_8

--- src/main/lesson9/TreeMapTest.java
+++ src/main/lesson9/TreeMapTest.java
@@ -1,5 +1,9 @@
 package src.main.lesson9;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -8,10 +12,12 @@ import java.util.*;
  * @Version 1.0
  */
 public class TreeMapTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //func1();
         //func2();
-        func3();
+        //func3();
+        func4();
+        func5();
     }
 
     // SortedMap，它在内部会对Key进行排序。注意到SortedMap是接口，它的实现类是TreeMap。
@@ -71,7 +77,41 @@ public class TreeMapTest {
             System.out.println(stringStudentSortedMap.get(key));    // aaa ccc bbb dddd
         }
     }
+
+    // 测试配置文件
+    // 由于历史遗留原因，Properties内部本质上是一个Hashtable,它的设计实际上是有问题的，但是为了保持兼容性，现在已经没法修改了
+    // Java默认配置文件以.properties为扩展名，每行以key=value表示，以#课开头的是注释
+    public static void func4() throws IOException {
+        // String f = "E:\\githubShyzhen\\javaLearningDialy\\src\\demo.properties";
+        String f1 = "src/demo.properties";
+        String f2 = "src/new.properties";
+
+        Properties properties = new Properties();
+        properties.load(new java.io.FileInputStream(f1));
+
+        // 果有多个.properties文件，可以反复调用load()读取，后读取的key-value会覆盖已读取的key-value
+        // 可以把默认配置文件放到classpath中，然后，根据机器的环境编写另一个配置文件，覆盖某些默认的配置
+        properties.load(new java.io.FileInputStream(f2));  // properties.load(new java.io.FileReader(f2, StandardCharsets.UTF_8));
+
+
+        String cdnUrl = properties.getProperty("cdn_url");
+        String tokenTtl = properties.getProperty("token_ttl");
+
+        System.out.println(cdnUrl);
+    }
+
+    // 写入配置文件 setProperty  store
+    // 完全覆写，相当于把以前的删掉重新生成新文件
+    public static void func5() throws IOException {
+        String f1 = "src/new2.properties";
+        Properties properties = new Properties();
+
+        properties.setProperty("admin_id", "99999");
+        properties.store(new java.io.FileOutputStream(f1), "这是写入的properties注释");
+    }
 }
+
+
 
 
 
